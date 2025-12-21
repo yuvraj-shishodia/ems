244e6f3d8f6967c29be3a54656f3bb6bf6c9a3c5
yuvraj-shishodia <yuvrajshishodia24@gmail.com>
2025-12-22 02:34:29 +0530

    setting up the project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import Login from './components/Auth/Login'
 
 const App = () => {
   return (
-    <div>App</div>
+<>
+<Login />
+</>
   )
 }
 
---
+++ src/components/Auth/Login.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Login = () => {
+  return (
+    <div className='flex items-center justify-center h-screen w-screen'>
+        <div className='border-2 border-red-600'>
+            <form className='flex flex-col justify-center items-center'>
+                <input type="email" placeholder='Enter your email' />
+                <input type="password" placeholder='Enter your password'/>
+                <button>Log in</button>
+            </form>
+        </div>
+    </div>
+  )
+}
+
+export default Login
