5c4aab1d39671fde4cdf3c94ba8160a7f2bf4e64
yuvraj-shishodia <yuvrajshishodia24@gmail.com>
2025-12-22 03:11:51 +0530

    login password ui setup

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -3,9 +3,19 @@ const Login = () => {
     <div className="flex items-center justify-center h-screen w-screen">
       <div className="border-2 border-red-600">
         <form className="flex flex-col justify-center items-center">
-          <input type="email" placeholder="Enter your email" />
-          <input type="password" placeholder="Enter your password" />
-          <button>Log in</button>
+          <input
+            className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl text-black outline-none bg-transparent placeholder-text-white"
+            type="email"
+            placeholder="Enter your email"
+          />
+          <input
+            className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl text-black mt-3"
+            type="password"
+            placeholder="Enter your password"
+          />
+          <button className="text-white border-emerald-600 rounded-full py-3 px-5 text-xl text-black outline-none bg-emerald-600 border-none placeholder-text-white">
+            Log in
+          </button>
         </form>
       </div>
     </div>
