901ddc43e8a2cc5cac0ce52f4cbbeed1e1ff02c3
yuvraj-shishodia <yuvrajshishodia24@gmail.com>
2025-12-24 01:05:58 +0530

    Login page comple

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,19 +1,44 @@
+import React, { useState } from "react";
+
 const Login = () => {
+
+   const [email, setEmail] = useState("");
+   const [password, setPassword] = useState("");
+   const submitHandler = (e) => {
+     e.preventDefault();
+     console.log("Email is:", email);
+     console.log("Password is:", password);
+     setEmail("");
+      setPassword("");
+   };
+
   return (
-    <div className="flex items-center justify-center h-screen w-screen">
+    <div className="flex items-center justify-center h-screen w-screen out">
       <div className="border-2 rounded-xl border-emerald-600 p-20">
-        <form className="flex flex-col justify-center items-center">
+        <form onSubmit={(e) => {
+          submitHandler(e)}}
+           className="flex flex-col justify-center items-center">
           <input
+          value={email}
+          onChange={(e)=>{
+            setEmail(e.target.value);
+          }}
+          required
             className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl outline-none bg-transparent placeholder-text-gray-400"
             type="email"
             placeholder="Enter your email"
           />
           <input
+          value={password}
+          onChange={(e)=>{
+            setPassword(e.target.value);
+          }}
+          required
             className="border-2 border-emerald-600 rounded-full py-3 px-5 text-xl mt-3 outline-none bg-transparent placeholder-text-gray-400"
             type="password"
             placeholder="Enter your password"
           />
-          <button className="text-white rounded-full py-3 px-5 mt-5 text-xl text-black outline-none bg-emerald-600 border-none placeholder-text-white">
+          <button className="text-white rounded-full py-3 px-5 mt-5 text-xl outline-none bg-emerald-600 border-none">
             Log in
           </button>
         </form>
