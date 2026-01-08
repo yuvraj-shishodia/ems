7c15ad9034c6b6e7aef790e382b98ff05b8aab8c
yuvraj-shishodia <yuvrajshishodia24@gmail.com>
2026-01-08 23:58:53 +0530

    basic login configuaration setUser

--- src/App.jsx
+++ src/App.jsx
@@ -14,12 +14,23 @@ const App = () => {
 const [user, setUser] = useState(null)
 
 const handleLogin = (email, password) => {
-
+  if(email == 'admin@me.com' && password == '123'){
+    setUser('admin')
+    console.log(user);
+    
+  }
+  else if (email == "user@me.com" && password == "123") {
+    setUser("employee");
+    console.log(user);
+    
+  } else {
+    alert("Invalid Credentials");
+  }
 }
 
   return (
     <>
-      {!user ? <Login /> : ''}
+      {!user ? <Login handleLogin={handleLogin} /> : ''}
       {/* <EmployeeDashboard /> */}
       {/* <AdminDashboard /> */}
     </>
